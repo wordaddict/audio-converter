5788cc9a981d93206e6ba64821d80a1602103947
wordaddict_ <madeyinka@terragonltd.com>
2018-08-17 13:14:20 +0100

    base server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const router = require('./router/index');
+
+app.use('/', router);
+
+app.get('/', (req, res) => {
+    res.json('Welcome to the audio api');
+});
+
+const port = 3005;
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
