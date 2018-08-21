4079007d6237f5f3b34415e5427984ed026e254d
wordaddict_ <madeyinka@terragonltd.com>
2018-08-21 19:16:42 +0100

    done with the basic application

--- app/router/index.js
+++ app/router/index.js
@@ -30,9 +30,6 @@ router.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
 router.post('/audio', (req, res, next) => {
     upload(req, res, (err) => {
-        console.log('files', req.files)
-        console.log('file body', req.body)
-        console.log('file', req.file)
         if (err) {
               res.json({ error_code: 1, err_desc: err })
             return;
@@ -55,12 +52,13 @@ router.post('/audio', (req, res, next) => {
            ans = JSON.parse(ans);
            const data = ans.results[0].alternatives[0].transcript;
           fs.unlinkSync(req.file.path);//remove the file
-          res.send({
-              code: 200,
-              error: false,
-              message: 'audio converted successfully',
-              response: data
-          })
+        //   res.send({
+        //       code: 200,
+        //       error: false,
+        //       message: 'audio converted successfully',
+        //       response: data
+        //   })
+        res.render('audio', {text: data})
          });
     });
 })
