e7c598e3150e1962e870e8604e41f55ff2a5a809
wordaddict_ <madeyinka@terragonltd.com>
2018-08-23 00:19:47 +0100

    added check for large files

--- router/index.js
+++ router/index.js
@@ -8,6 +8,7 @@ const time = Date.now();
 const fs = require('fs');
 const config = require('../config/config');
 const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
 
 const apiUrl = config.apiUrl;
 const apiAuth = config.apiAuth;
@@ -27,9 +28,15 @@ const upload = multer({ storage }).single('file');
 
 router.post('/audio', (req, res, next) => {
     upload(req, res, (err) => {
-      console.log('reqfile', req.file);
+      const filePath = req.file.path;
+      const fileName = filePath.split('/');
+      const newFileN = fileName[1];
+      const fileSize = req.file.size;
+      const fileSizeInMb = fileSize / 1048576;
       if (req.file === undefined){
         return res.send({
+          error: true,
+          code: 400,
           message: "unable to get file"
         })
       }
@@ -45,36 +52,57 @@ router.post('/audio', (req, res, next) => {
               res.json({ error_code: 2, err_desc: err })
             return;
         }
-        unirest.post(url)
-        .headers({ 'Content-Type': `audio/${extention}` })
-        .headers({ 'Authorization': apiAuth })
-        .field('filename', newFileName)
-        .attach('file', req.file.path) // Attachment
-        .end(function(response) {
-          const responseCode = response.statusCode;
-          if (responseCode === 400){
-            return res.send({
-              error: true,
-              code: 400,
-              message: 'file type not acceptable'
-            })
-          }
+        let fileP = '';
+        if (fileSizeInMb > 1) {
+          fileP = `largeFiles/output.mp3`
+          ffmpeg(`./uploads/${newFileN}`)
+            .inputOptions('-t 7') // 7s
+            .output('./largeFiles/output.mp3')
+            .run()
+            
+            return makeAPICall()
+              .then(() => {
+              });
+        };
+        fileP = req.file.path;
+        async function makeAPICall(){
+          return unirest.post(url)
+          .headers({ 'Content-Type': `audio/${extention}` })
+          .headers({ 'Authorization': apiAuth })
+          .field('filename', newFileName)
+          .attach('file', fileP) // Attachment
+          .end(function(response) {
+            const responseCode = response.statusCode;
+            if (responseCode === 400){
+              return res.send({
+                error: true,
+                code: 400,
+                message: 'file type not acceptable'
+              })
+            }
+  
+            if (responseCode === 404) {
+              return res.send({
+                error: true,
+                code: 404,
+                message: 'transcription not found'
+              })
+            }
+  
+              let ans = response.raw_body;
+             ans = JSON.parse(ans);
+             const data = ans.results[0].alternatives[0].transcript;
+            fs.unlinkSync(req.file.path);//remove the file
+          res.render('audio', {text: data})
+           });
+        }
 
-          if (responseCode === 404) {
-            return res.send({
-              error: true,
-              code: 404,
-              message: 'transcription not found'
-            })
-          }
+        makeAPICall()
+        .then((data) => {
+        });
 
-            let ans = response.raw_body;
-           ans = JSON.parse(ans);
-           const data = ans.results[0].alternatives[0].transcript;
-          fs.unlinkSync(req.file.path);//remove the file
-        res.render('audio', {text: data})
-         });
     });
+
 })
 
 module.exports = router;
